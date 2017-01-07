420cf5c391b0d54c77221c61d412068fd11561b2
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2017-01-07 20:21:59 +0100

    added comments for camera

--- src/engine/Camera.cpp
+++ src/engine/Camera.cpp
@@ -1,8 +1,18 @@
 #include "Camera.h"
 
 
+/**
+ * Constructor
+ */
 Camera::Camera(float x, float y) : Instance(x, y) {}
 
+/**
+ * Update logic for camera
+ */
 void Camera::tick(float delta) {}
 
+/**
+ * This function is suppost to be used to draw
+ * GUI
+ */
 void Camera::draw(float delta) {}
