688c9a679febdcae337e71d4de9000bfe7730087
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2017-01-22 18:39:26 +0100

    added possibility in develop.sh to compile applications from example repository submodule

--- develop.sh
+++ develop.sh
@@ -1,9 +1,18 @@
+app=./testgame
+
+if [ -z ${1+x} ]; then
+    echo "Using default"
+    #echo "var is unset"; exit 1;
+else
+    app=NINAGE-examples/$1;
+fi
+
 cd libninage
 make install
 
 cd ..
 
-cd testgame
+cd $app
 make
 ./game.out
 
