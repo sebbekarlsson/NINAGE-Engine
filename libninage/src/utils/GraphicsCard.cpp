81ccca5e45d794393b1dac04a04cc532b31de1fa
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2017-07-17 23:57:54 +0200

    codestyle fixes

--- libninage/src/utils/GraphicsCard.cpp
+++ libninage/src/utils/GraphicsCard.cpp
@@ -24,24 +24,25 @@ void GraphicsCard::drawText(
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
     
-    if (!app->isFontLoaded(fontfile)) {
+    if (!app->isFontLoaded(fontfile))
         app->loadFont(fontfile, size);
-    }
 
     TTF_Font& font = *app->fonts->find(fontfile)->second;
 
     if (app->fonts->find(fontfile)->second == NULL) {
         printf("TTF ERROR: %s", TTF_GetError());
+        
         return;
     }
 
     SDL_Surface * sFont = TTF_RenderText_Blended(
-            &font, message.c_str(),
-            {(Uint8)color->r, (Uint8)color->g, (Uint8)color->b}
-            );
+        &font, message.c_str(),
+        {(Uint8)color->r, (Uint8)color->g, (Uint8)color->b}
+    );
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
     glTexImage2D(
         GL_TEXTURE_2D,
         0,
@@ -55,10 +56,19 @@ void GraphicsCard::drawText(
     );
 
     glBegin(GL_QUADS);
-    glTexCoord2f(0,0); glVertex2f(0, 0);
-    glTexCoord2f(1,0); glVertex2f(sFont->w, 0);
-    glTexCoord2f(1,1); glVertex2f(sFont->w, sFont->h);
-    glTexCoord2f(0,1); glVertex2f(0, sFont->h);
+    
+    glTexCoord2f(0,0);
+    glVertex2f(0, 0);
+
+    glTexCoord2f(1,0);
+    glVertex2f(sFont->w, 0);
+    
+    glTexCoord2f(1,1);
+    glVertex2f(sFont->w, sFont->h);
+    
+    glTexCoord2f(0,1);
+    glVertex2f(0, sFont->h);
+    
     glEnd();
 
     glDeleteTextures(1, &texture);
