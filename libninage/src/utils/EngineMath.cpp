4ccc03ef6bb9d1b087ef3c9e2f518edc34446a6b
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2017-07-18 21:39:39 +0200

    added documentation for my new method

--- libninage/src/utils/EngineMath.cpp
+++ libninage/src/utils/EngineMath.cpp
@@ -1,10 +1,27 @@
 #include "../includes/utils/EngineMath.h"
 
 
+/**
+ * Turns degrees to radians
+ *
+ * @param float degrees
+ *
+ * @return float
+ */
 float EngineMath::toRadians(float degrees) {
     return degrees * (M_PI / 180);
 }
 
+/**
+ * Mainly used for physics.
+ * This method will decrease the value of a variable until
+ * it goes to `0` (zero).
+ *
+ * @param x
+ * @param friction
+ *
+ * @return float
+ */
 float EngineMath::toZero(float x, float friction) {
    if (x > 0) {
         if (x - friction < 0) {
