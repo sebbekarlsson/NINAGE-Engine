a505a4414a55e198cee2add9bcc47aafc6f080bc
Sebastian karlsson <sebbekarlsson97@gmail.com>
2017-10-11 20:03:11 +0200

    started working on modelloader cache

--- libninage/src/includes/utils/ModelLoader.h
+++ libninage/src/includes/utils/ModelLoader.h
@@ -2,14 +2,19 @@
 #define MODELLOADER_H
 
 #include <iostream>
+#include <map>
 #include "Model3D.h"
 
 
 class ModelLoader {
     public:
+        std::map<std::string, Model3D*> *models;
+        
         ModelLoader();
+        ~ModelLoader();
 
         Model3D* load(std::string filepath);
+        void clearModelcache();
 };
 
 #endif
--- libninage/src/utils/ModelLoader.cpp
+++ libninage/src/utils/ModelLoader.cpp
@@ -3,6 +3,10 @@
 
 ModelLoader::ModelLoader() {}
 
+ModelLoader::~ModelLoader(void) {
+    delete this->models;
+}
+
 /**
  * Load a 3D model
  *
@@ -11,6 +15,21 @@ ModelLoader::ModelLoader() {}
  * @return Model3D
  */
 Model3D* ModelLoader::load(std::string filepath) {
+    if (this->models->count(filepath))
+        return this->models->find(filepath)->second;
+    
     Model3D* model = new Model3D();
+    
+    this->models->operator[](filepath) = model; // store model in the map
+
     return model; // not implemented yet
+}
+
+/**
+ * Clear the models vector cache
+ *
+ * @return void
+ */
+void ModelLoader::clearModelcache() {
+    this->models->clear();
 }
