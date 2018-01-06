ca3f441ae0abf55c8e3b478703f061d94e1c9dc9
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-01-06 12:21:52 +0100

    cleanup

--- libninage/src/utils/Model3D.cpp
+++ libninage/src/utils/Model3D.cpp
@@ -7,20 +7,30 @@ Model3D::Model3D() {}
 void Model3D::draw() {
     std::vector<std::vector<float>>::iterator it;
     std::vector<std::vector<int>>::iterator faceit;
-    
+
     glDisable(GL_TEXTURE_2D);
-    //glColor3f(255, 255, 255);
-    
+
     int u = 0;
     for (faceit = this->faces.begin(); faceit != this->faces.end(); ++faceit) {
         glBegin(GL_TRIANGLES);
-            for (int index = 0; index < 3; index++) {
-                glNormal3f(this->vertexNormals[this->vertexNormalsIndices[u][index]-1][0], this->vertexNormals[this->vertexNormalsIndices[u][index]-1][1], this->vertexNormals[this->vertexNormalsIndices[u][index]-1][2]);
-                //glNormal3f(this->vertexNormals[(*faceit)[index]-1][0], this->vertexNormals[(*faceit)[index]-1][1], this->vertexNormals[(*faceit)[index]-1][2]);
 
+        for (int index = 0; index < 3; index++) {
 
-                glVertex3f(this->vertices[(*faceit)[index]-1][0], this->vertices[(*faceit)[index]-1][1], this->vertices[(*faceit)[index]-1][2]);
-           }
+            int indexPointer = (*faceit)[index] - 1;
+
+            glNormal3f(
+                this->vertexNormals[indexPointer][0], // x
+                this->vertexNormals[indexPointer][1], // y
+                this->vertexNormals[indexPointer][2] // z
+            );
+
+            glVertex3f(
+                this->vertices[indexPointer][0], // x
+                this->vertices[indexPointer][1], // y
+                this->vertices[indexPointer][2] // z
+            );
+        }
+
         glEnd();
 
         u++;
