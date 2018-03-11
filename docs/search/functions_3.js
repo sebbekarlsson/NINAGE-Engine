69b81977371630424fc773dcd2775f18cd5b32f9
sebastian karlsson <sebbekarlsson97@gmail.com>
2018-03-11 19:56:59 +0100

    updated docs

--- docs/search/functions_3.js
+++ docs/search/functions_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['destantiate',['destantiate',['../class_state.html#a84cb707ac148dffbeeb94f6bbe70965e',1,'State']]],
-  ['draw',['draw',['../class_camera.html#a344d3140aeb78cd56a3fa067b9e9ab6e',1,'Camera::draw()'],['../class_collision_box.html#a70245cc2187113cbb9f4245abdb6bbb9',1,'CollisionBox::draw()'],['../class_illustration_stack.html#a1cdb256a5a819356bb3438ba88ec8aa2',1,'IllustrationStack::draw()'],['../class_instance.html#aa089785150fa14305bd0c04f88787b85',1,'Instance::draw()'],['../class_ninage.html#aa3b5968b4b5fa2adb48a88624d3abbcf',1,'Ninage::draw()'],['../class_scene.html#aacd9c2e0f95b243dea4abe041fb1f624',1,'Scene::draw()'],['../class_model3_d.html#adc0febeb1c52ffc4beac2299d3c1b12d',1,'Model3D::draw()']]],
+  ['draw',['draw',['../class_camera.html#a344d3140aeb78cd56a3fa067b9e9ab6e',1,'Camera::draw()'],['../class_collision_box.html#a70245cc2187113cbb9f4245abdb6bbb9',1,'CollisionBox::draw()'],['../class_illustration_stack.html#a1cdb256a5a819356bb3438ba88ec8aa2',1,'IllustrationStack::draw()'],['../class_instance.html#aa089785150fa14305bd0c04f88787b85',1,'Instance::draw()'],['../class_model3_d.html#adc0febeb1c52ffc4beac2299d3c1b12d',1,'Model3D::draw()'],['../class_ninage.html#aa3b5968b4b5fa2adb48a88624d3abbcf',1,'Ninage::draw()'],['../class_scene.html#aacd9c2e0f95b243dea4abe041fb1f624',1,'Scene::draw()']]],
   ['drawdefault',['drawDefault',['../class_instance.html#a815c5985c12cc8f740c1ad435c345caf',1,'Instance::drawDefault()'],['../class_scene.html#ad724c43fb70f599ffe0df6e8a0589580',1,'Scene::drawDefault()']]],
   ['drawinstances',['drawInstances',['../class_state.html#a342032c13ab62476608d7676d03af7e9',1,'State']]]
 ];
