fc821ba54da74739028d014627525a94909d89e7
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-06-04 11:46:32 +0200

    updated documentation

--- docs/search/all_e.js
+++ docs/search/all_e.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['unbind',['unbind',['../class_illustration.html#a7b8473937c8ff066f41099cc113d0f29',1,'Illustration']]],
-  ['updatephysics',['updatePhysics',['../class_entity.html#ad37218828d4edf474f7c709532a11d95',1,'Entity']]]
+  ['updatephysics',['updatePhysics',['../class_entity.html#a1d5c1a39b1b32111433c4a8695136320',1,'Entity']]]
 ];
--- docs/search/functions_4.js
+++ docs/search/functions_4.js
@@ -5,7 +5,7 @@ var searchData=
   ['getfps',['getFPS',['../class_ninage.html#a5c647c44ad143de2d1377b17b75c9014',1,'Ninage']]],
   ['getheight',['getHeight',['../class_illustration.html#a3979a5918b8f889156327215b945824e',1,'Illustration::getHeight()'],['../class_illustration_stack.html#a05f3a4af00f0094f62a892fec80ae6fc',1,'IllustrationStack::getHeight()'],['../class_ninage.html#a4038a3f3833b7f9e702bc1f907c8493d',1,'Ninage::getHeight()']]],
   ['getmouseposition',['getMousePosition',['../class_ninage.html#aa68017330283ad493daf9725555562f3',1,'Ninage']]],
-  ['getmovingdirection',['getMovingDirection',['../class_entity.html#a18f75d44b005bcd9ec3737a64f5eaa3d',1,'Entity']]],
+  ['getmovingdirection',['getMovingDirection',['../class_entity.html#ae5f567a71523008e00f48b7216a772c3',1,'Entity']]],
   ['getviewmode',['getViewmode',['../class_ninage.html#a3a688f6fd9ac6b54b4e260171d17cd73',1,'Ninage']]],
   ['getwidth',['getWidth',['../class_illustration.html#a3835bad57f6e52d97838e6448a8ea14c',1,'Illustration::getWidth()'],['../class_illustration_stack.html#a49b802d27f706beee344f9fa170161f7',1,'IllustrationStack::getWidth()'],['../class_ninage.html#aa0a09948b1563b4a44cc3f9ae5829683',1,'Ninage::getWidth()']]],
   ['getx',['getX',['../class_instance.html#a0301b8dea0d34f448961570b22b3805e',1,'Instance']]],
